c24103ffe59fb4d98058fbededfb98c3fca048b1
astastya <ey20699@bristol.ac.uk>
2021-10-18 16:40:13 +0100

    I do not know

--- bottlesofbeer/bottlesofbeer.go
+++ bottlesofbeer/bottlesofbeer.go
@@ -1,20 +1,101 @@
-package main
+package bottlesofbeer
+
 import (
+	//"bufio"
 	"flag"
-//	"net/rpc"
-//	"fmt"
-//	"time"
-//	"net"
+	"fmt"
+	"net"
+	"net/rpc"
+	"os"
+	"time"
+	//	"net/rpc"
+	//	"fmt"
+	//	"time"
+	//	"net"
 )
 
+var PassHandler = "passOperation.DealPass"
+var FinishHandler = " passOperation.DealFinish"
 var nextAddr string
 
+type passOperation struct{ targetIP string }
+type pass struct {
+	Number int
+}
 
-func main(){
-//	thisPort := flag.String("this", "8030", "Port for this process to listen on")
-	flag.StringVar(&nextAddr, "next", "localhost:8040", "IP:Port string for next member of the round.")
-//	bottles := flag.Int("n",0, "Bottles of Beer (launches song if not 0)")
+func (p *passOperation) DealPass(p1 pass) (err error) {
+	if p1.Number > 0 {
+		fmt.Println(p1.Number, " bottles of beer on the wall,", p1.Number, " bottles of beer. Take one down, pass it around...")
+		e1 := client.Call(PassHandler, pass{Number: p1.Number - 1}, nil)
+
+		if e1 != nil {
+			fmt.Println(e1)
+			return
+		}
+	} else {
+		fmt.Println("all has been taken")
+		client.Call(FinishHandler, nil, nil)
+		os.Exit(2)
+	}
+	return
+}
+func (p *passOperation) DealFinish(p1 pass) (err error) {
+	fmt.Println("all has been taken")
+	os.Exit(2)
+	return
+}
+
+func main() {
+	thisPort := flag.String("this", "8030", "Port for this process to listen on")
+	flag.StringVar(&nextAddr, "next", "localhost:8030", "IP:Port string for next member of the round.")
+	bottles := flag.Int("n", 0, "Bottles of Beer (launches song if not 0)")
+	target := flag.String("target", "127.0.0.1:8030", "the ip address for the target process")
 	flag.Parse()
+	err := rpc.Register(&passOperation{targetIP: *target})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	//TODO: Up to you from here! Remember, you'll need to both listen for
 	//RPC calls and make your own.
+
+	listener, _ := net.Listen("tcp", ":"+*thisPort)
+	defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}(listener)
+
+	if *bottles != 0 {
+		for i := 0; i < 10; i++ {
+			time.Sleep(1 * time.Second)
+			fmt.Println("sleep for turn:", i)
+			client, _ = rpc.Dial("tcp", *target)
+		}
+	}
+
+	rpc.Accept(listener)
+
+	if *bottles == 0 {
+		client, _ = rpc.Dial("tcp", *target)
+		defer func(client *rpc.Client) {
+			err := client.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}(client)
+	}
+
+	if *bottles != 0 {
+		errC := client.Call(PassHandler, pass{Number: 0}, nil)
+		if errC != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
+
+var client *rpc.Client
--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,16 +1,42 @@
 package main
+
 import (
-//	"net/rpc"
+	"bufio"
 	"flag"
-//	"bufio"
-//	"os"
-//	"secretstrings/stubs"
 	"fmt"
+	"net/rpc"
+	"os"
+	"secretstrings/stubs"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
+	client, _ := rpc.Dial("tcp", *server)
+	defer func(client *rpc.Client) {
+		err := client.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(client)
+	dat, _ := os.Open("wordlist")
+	datReader := bufio.NewReader(dat)
+LOOP:
+	for {
+		readString, err := datReader.ReadString('\n')
+		if err != nil {
+			break LOOP
+		}
+		request := stubs.Request{Message: readString}
+		response := new(stubs.Response)
+		errClientCall := client.Call(stubs.PremiumReverseHandler, request, response)
+		if errClientCall != nil {
+			fmt.Println(errClientCall)
+			return
+		}
+		fmt.Println("Responded:" + response.Message)
+	}
+
 	//TODO: connect to the RPC server and send the request(s)
 }
